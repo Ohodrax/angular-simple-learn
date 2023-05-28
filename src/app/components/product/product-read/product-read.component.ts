5a2b2610eb6c728e27f0aa60af9e44b2d28c1b9f
sparkmaster <rodrigo.cassiano00@gmail.com>
2023-05-28 15:51:23 -0300

    Creating new component product read, create and calling new function to get all products in backend and insert him into the products array

---
+++ src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,21 @@
+import { ProductService } from './../product.service';
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit{
+  products: Product[];
+
+  constructor(private ProductService: ProductService) {}
+
+  ngOnInit(): void {
+    this.ProductService.read().subscribe(products => {
+      this.products = products;
+    })
+  }
+}
